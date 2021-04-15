19a0d15801227f6fe625de8d694c8cc160ed095b
hyungjin kang <hyungjin.s.kang@gmail.com>
2021-04-15 13:37:35 -0500

    End of section 7

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,27 +1,31 @@
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { HomeComponent } from './home/home.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
 {path: '', component: HomeComponent},
-{
-  path: '', 
+{ path: '', 
   runGuardsAndResolvers: 'always',
   canActivate: [AuthGuard],
   children: [
-
     {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
     {path: 'members/:id', component: MemberDetailComponent},
     {path: 'lists', component: ListsComponent},
     {path: 'messages', component: MessagesComponent}
   ]
 },
-{path: '**', component: HomeComponent, pathMatch: 'full'},
+{path: 'errors', component: TestErrorsComponent},
+{path: 'not-found', component: NotFoundComponent },
+{path: 'server-error', component: ServerErrorComponent},
+{path: '**', component: NotFoundComponent, pathMatch: 'full'},
 
 ];
 
