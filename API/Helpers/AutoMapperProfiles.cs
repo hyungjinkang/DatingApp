7104ab4909b160125185889e865d8c65ba0e6d97
hyungjin kang <hyungjin.s.kang@gmail.com>
2021-04-19 16:30:59 -0500

    End of section 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => 
+                    src.Photos.FirstOrDefault(x => x.IsMain).Url)) //make MemberDto.photoUrl populated
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+                //Above line eliminates getAge method in AppUser and the queries for passwordHash, and passwordSalt
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -49,10 +49,10 @@ namespace API
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-            
+
             // if (env.IsDevelopment())
             // {
-            //     app.UseDeveloperExceptionPage();
+            //     //app.UseDeveloperExceptionPage();
             //     app.UseSwagger();
             //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             // }
